2c630e008e6697a9920572bacb5fca8658e420ee
ryo <ryotarousk@gmail.com>
2025-12-02 00:09:57 +0900

    aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body className="antialiased font-sans">
         <SessionProvider>
           <AnalyticsInit />
